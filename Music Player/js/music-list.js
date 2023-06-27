0d8e3c02444e4b7342a72e4d0166b152c3816407
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2023-06-27 10:32:30 +0530

    Add files via upload

---
+++ Music Player/js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "Kanye West - Heartless",
+        artist: "Kanye West",
+        img: "music2",
+        src: "music1"
+    },
+    {
+        name: "Louis Fonsi & Daddy Yankee - Despacito",
+        artist: "Louis Fonsi & Daddy Yankee",
+        img: "music1",
+        src: "music2"
+    },
+    {
+        name: "Interworld - Metamorphosis",
+        artist: "Interworld",
+        img: "music3",
+        src: "music3"
+    },
+    {
+        name: "Lil Tecca - Ransom",
+        artist: "Lil Tecca",
+        img: "music4",
+        src: "music4"
+    },
+    {
+        name: "Emiway Bantai, Jassa Dhillon, Honey Singh, Harnoor - Desi Kalakaar Megamix",
+        artist: "Emiway Bantai, Jassa Dhillon, Honey Singh, Harnoor",
+        img: "music5",
+        src: "music5"
+    },
+    {
+        name: "Omprakash Mishra - Bol naa Aunty",
+        artist: "Omprakash Mishra",
+        img: "music6",
+        src: "music6"
+    }
+
+]
